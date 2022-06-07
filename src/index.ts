ee0a74dac7ba7aafbe9f0dd97b90a695496da51f
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2022-06-07 23:30:08 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,12 @@ const settings = settingsProvider.GetSettings()
 console.log(`settings:${JSON.stringify(settings)}`)
 
 const restClient = ghClientProvider.getOctokit(settings.ghToken)
-const commits = await restClient.rest.pulls.listCommits({
-    owner: settings.repoOwner,
-    repo: settings.repoName,
-    pull_number: settings.prNumber
-})
+// const commits = await restClient.rest.pulls.listCommits({
+//     owner: settings.repoOwner,
+//     repo: settings.repoName,
+//     pull_number: settings.prNumber
+// })
 
-commits.data.forEach(x => {
-    console.log(`commit msg: ${x.commit}`)
-})
+// commits.data.forEach(x => {
+//     console.log(`commit msg: ${x.commit}`)
+// })
